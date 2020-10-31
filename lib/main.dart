0bbcd18391f6c2803c6e7cb1e6b87b15020b2c22
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-31 19:00:31 +0200

    feat: restructure

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
 import "src/App_Header.dart";
 
@@ -12,7 +11,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // FlutterStatusbarcolor.setStatusBarColor(Colors.red);
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.black),
         home: Scaffold(
